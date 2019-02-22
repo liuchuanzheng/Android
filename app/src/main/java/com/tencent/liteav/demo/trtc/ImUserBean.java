4d0a6aa16a0657760edb9423fb7b5e08392f6c51
liuchuanzheng <liuchuanzheng@feiliu.com>
2019-02-22 18:12:52 +0800

    可以呼叫了

---
+++ app/src/main/java/com/tencent/liteav/demo/trtc/ImUserBean.java
@@ -0,0 +1,19 @@
+package com.tencent.liteav.demo.trtc;
+
+/**
+ * @author 刘传政
+ * @date 2019/2/22 0022 15:27
+ * QQ:1052374416
+ * 电话:18501231486
+ * 作用:
+ * 注意事项:
+ */
+public class ImUserBean {
+    public String userName;
+    public String userSig;
+
+    public ImUserBean(String userName, String userSig) {
+        this.userName = userName;
+        this.userSig = userSig;
+    }
+}
