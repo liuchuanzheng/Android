794ab543dceb05d915c3db361635cd7f3366a0d0
liuchuanzheng <liuchuanzheng@feiliu.com>
2019-02-25 20:14:36 +0800

    优化demo页面

--- app/src/main/java/com/tencent/liteav/demo/TRTCApplication.java
+++ app/src/main/java/com/tencent/liteav/demo/TRTCApplication.java
@@ -1,18 +1,106 @@
 package com.tencent.liteav.demo;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
+
+import com.tencent.liteav.demo.trtc.ImUserBean;
+import com.tencent.liteav.demo.trtc.TRTCGetUserIDAndUserSig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TRTCApplication extends MultiDexApplication {
+    public static List<ImUserBean> imUserBeanList;
+    public static ImUserBean loginImUserBean;
+    private static TRTCApplication mInstance;
+    private Activity app_activity = null;
 
     @Override
     public void onCreate() {
 
         super.onCreate();
-//        initTencentIM();
-
+        readUsers();
+        mInstance = this;
+        initGlobeActivity();
     }
 
+    private void readUsers() {
+        // 如果配置有config文件，则从config文件中选择userId
+        TRTCGetUserIDAndUserSig mUserInfoLoader = new TRTCGetUserIDAndUserSig(this);
+        final ArrayList<String> userIds = mUserInfoLoader.getUserIdFromConfig();
+        final ArrayList<String> userSigs = mUserInfoLoader.getUserSigFromConfig();
 
+        imUserBeanList = new ArrayList<>();
+        if (userIds != null && userIds.size() > 0) {
+            for (String userId : userIds) {
+                ImUserBean imUserBean = new ImUserBean("", "");
+                imUserBean.userName = userId;
+                imUserBeanList.add(imUserBean);
+            }
+            for (int i = 0; i < imUserBeanList.size(); i++) {
+                imUserBeanList.get(i).userSig = userSigs.get(i);
+            }
+        }
+    }
 
+    private void initGlobeActivity() {
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                app_activity = activity;
+                Log.e("onActivityCreated===", app_activity + "");
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                Log.e("onActivityDestroyed===", app_activity + "");
+            }
+
+            /** Unused implementation **/
+            @Override
+            public void onActivityStarted(Activity activity) {
+                app_activity = activity;
+                Log.e("onActivityStarted===", app_activity + "");
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+                app_activity = activity;
+                Log.e("onActivityResumed===", app_activity + "");
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                Log.e("onActivityPaused===", app_activity + "");
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                Log.e("onActivityStopped===", app_activity + "");
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            }
+        });
+    }
+
+    /**
+     * 获取实例
+     *
+     * @return
+     */
+    public static TRTCApplication getInstance() {
+        return mInstance;
+    }
+
+    /**
+     * 公开方法，外部可通过 MyApplication.getInstance().getCurrentActivity() 获取到当前最上层的activity
+     */
+    public Activity getCurrentActivity() {
+        return app_activity;
+    }
 
 }
--- app/src/main/java/com/tencent/liteav/demo/trtc/TRTCNewActivity.java
+++ app/src/main/java/com/tencent/liteav/demo/trtc/TRTCNewActivity.java
@@ -8,15 +8,28 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
+import com.tencent.imsdk.TIMCallBack;
+import com.tencent.imsdk.TIMElem;
+import com.tencent.imsdk.TIMElemType;
+import com.tencent.imsdk.TIMLogLevel;
+import com.tencent.imsdk.TIMManager;
+import com.tencent.imsdk.TIMMessage;
+import com.tencent.imsdk.TIMMessageListener;
+import com.tencent.imsdk.TIMSdkConfig;
+import com.tencent.imsdk.TIMTextElem;
 import com.tencent.liteav.demo.R;
+import com.tencent.liteav.demo.TRTCApplication;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -91,6 +104,8 @@ public class TRTCNewActivity extends Activity {
                     final EditText etUserId = (EditText)findViewById(R.id.et_user_name);
                     etUserId.setText(userIds.get(position));
                     etUserId.setEnabled(false);
+                    TRTCApplication.loginImUserBean = TRTCApplication.imUserBeanList.get(position);
+                    initTencentIM();
                 }
             });
             dialog.show();
@@ -318,6 +333,9 @@ public class TRTCNewActivity extends Activity {
             if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
                 permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
             }
+            if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.VIBRATE)) {
+                permissions.add(Manifest.permission.VIBRATE);
+            }
             if (permissions.size() != 0) {
                 ActivityCompat.requestPermissions(TRTCNewActivity.this,
                         (String[]) permissions.toArray(new String[0]),
@@ -343,6 +361,82 @@ public class TRTCNewActivity extends Activity {
             default:
                 break;
         }
+    }
+
+    private void initTencentIM() {
+        //初始化 SDK 基本配置
+        TIMSdkConfig config = new TIMSdkConfig(1400186741)
+//                .setAccoutType(accountType)
+                .enableLogPrint(true)              // 是否在控制台打印Log?
+                .setLogLevel(TIMLogLevel.DEBUG)    // Log输出级别（debug级别会很多）
+                .setLogPath(Environment.getExternalStorageDirectory().getPath() + "/justfortest/");
+        // Log文件存放在哪里？
+
+        //初始化 SDK
+        TIMManager.getInstance().init(getApplicationContext(), config);
+
+        TIMManager.getInstance().login(TRTCApplication.loginImUserBean.userName, TRTCApplication.loginImUserBean.userSig, new TIMCallBack() {
+            @Override
+            public void onError(int code, String desc) {
+                //错误码 code 和错误描述 desc，可用于定位请求失败原因
+                //错误码 code 列表请参见错误码表
+                Log.d("liuchuanzheng", "login failed. code: " + code + " errmsg: " + desc);
+            }
+
+            @Override
+            public void onSuccess() {
+                Log.d("liuchuanzheng", "login succ");
+                Toast.makeText(TRTCNewActivity.this,"im登录成功",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //设置消息监听器，收到新消息时，通过此监听器回调
+        TIMManager.getInstance().addMessageListener(new TIMMessageListener() {//消息监听器
+            @Override
+            public boolean onNewMessages(List<TIMMessage> msgs) {//收到新消息
+
+                    //当前界面 不用发消息了,就是自己用
+                    for (TIMMessage msg : msgs) {
+                        for (int i = 0; i < msg.getElementCount(); ++i) {
+                            TIMElem elem = msg.getElement(i);
+
+                            //获取当前元素的类型
+                            TIMElemType elemType = elem.getType();
+                            if (elemType == TIMElemType.Text) {
+                                //处理文本消息
+                                TIMTextElem timTextElem = (TIMTextElem) elem;
+                                String text = timTextElem.getText();
+                                if ("请求视频呼叫".equals(text)) {
+                                    Intent intent = new Intent(TRTCNewActivity.this, CallActivity.class);
+                                    intent.putExtra("type", "called");
+                                    intent.putExtra("username", msg.getSender());
+                                    startActivity(intent);
+                                }else if("我接听".equals(text)){
+                                    final Intent intent = new Intent(TRTCNewActivity.this, TRTCCallVideoActivity.class);
+                                    intent.putExtra("roomId", 999);
+                                    intent.putExtra("userId", TRTCApplication.loginImUserBean.userName);
+                                    intent.putExtra("sdkAppId", 1400186741);
+                                    intent.putExtra("userSig",TRTCApplication.loginImUserBean.userSig );
+                                    intent.putExtra("toUserName",msg.getSender());
+                                    startActivity(intent);
+                                }else if("挂断".equals(text)){
+                                    //消息的内容解析请参考消息收发文档中的消息解析说明
+                                    MessageEvent messageEvent = new MessageEvent();
+                                    messageEvent.msgs = msgs;
+                                    EventBus.getDefault().post(messageEvent);
+                                }
+                            } else if (elemType == TIMElemType.Image) {
+                                //处理图片消息
+                            }//...处理更多消息
+                        }
+                    }
+
+
+
+
+                return true; //返回true将终止回调链，不再调用下一个新消息监听器
+            }
+        });
     }
 
 }
